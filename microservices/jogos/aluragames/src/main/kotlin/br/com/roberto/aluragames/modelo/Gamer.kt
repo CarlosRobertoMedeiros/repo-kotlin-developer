4b90fa356dbe97f847e64a9953080b8937f06977
Carlos Roberto <carlosmedeiroslima@gmail.com>
2024-12-11 00:07:27 -0300

    feat: Implementing extension functions, companion objects and others

--- microservices/jogos/aluragames/src/main/kotlin/br/com/roberto/aluragames/modelo/Gamer.kt
+++ microservices/jogos/aluragames/src/main/kotlin/br/com/roberto/aluragames/modelo/Gamer.kt
@@ -1,5 +1,6 @@
 package br.com.roberto.aluragames.modelo
 
+import java.util.Scanner
 import kotlin.random.Random
 
 data class Gamer(var nome:String , var email:String){
@@ -13,6 +14,7 @@ data class Gamer(var nome:String , var email:String){
         }
     var idInterno:String? = null
         private set
+    val jogosBuscados = mutableListOf<Jogo?>()
 
     constructor(nome: String, email: String, dataNascimento:String, usuario:String) :
         this(nome,email){
@@ -20,6 +22,13 @@ data class Gamer(var nome:String , var email:String){
             this.usuario = usuario
             criarIdinterno()
         }
+
+    init {
+        if (nome.isBlank()){
+            throw IllegalArgumentException("Nome está em branco")
+        }
+        this.email = validarEmail()
+    }
 
     override fun toString(): String {
         return "Gamer(nome='$nome', email='$email', dataNascimento=$dataNascimento, usuario=$usuario, idInterno=$idInterno)"
@@ -32,5 +41,35 @@ data class Gamer(var nome:String , var email:String){
         idInterno = "$usuario#$tag"
     }
 
+    fun validarEmail() : String {
+        val regex = Regex("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$")
+
+        if (regex.matches(email)){
+            return email
+        }else {
+            throw IllegalArgumentException("Email inválido")
+        }
+
+    }
+
+    companion object {
+        fun criarGamer(leitura: Scanner): Gamer {
+            println("Boas vindas ao AluGames! Vamos fazer seu cadastro. Digite seu nome:")
+            val nome = leitura.nextLine()
+            println("Digite seu e-mail:")
+            val email = leitura.nextLine()
+            println("Deseja completar seu cadastro com usuário e data de nascimento? (S/N)")
+            val opcao = leitura.nextLine()
+
+            if (opcao.equals("s",true)){
+                println("Digite sua data de nascimento(DD/MM/AAAA):")
+                val nascimento = leitura.nextLine()
+                println("Digite seu nome de usuário:")
+                val usuario = leitura.nextLine()
+                return Gamer(nome, email, nascimento, usuario)
+            }
+            return Gamer(nome, email)
+        }
+    }
 
 }
--- microservices/jogos/aluragames/src/main/kotlin/br/com/roberto/aluragames/utilitario/StringExtension.kt
+++ microservices/jogos/aluragames/src/main/kotlin/br/com/roberto/aluragames/utilitario/StringExtension.kt
@@ -1,3 +1,11 @@
-fun String.transformarEmIdade(): Int {
+import java.time.LocalDate
+import java.time.Period
+import java.time.format.DateTimeFormatter
 
+fun String.transformarEmIdade(): Int {
+    val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+    val dataNascimento = LocalDate.parse(this, formatter)
+    val hoje = LocalDate.now()
+    val idade = Period.between(dataNascimento, hoje).years
+    return idade
 }
